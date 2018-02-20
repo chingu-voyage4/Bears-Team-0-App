217281a809b2e69e0d832dcc4049eaff5b53af73
Kevin Galang <kgalang2@gmu.edu>
2018-02-19 18:32:35 -0800

    Update quiz model constructor

--- models/quiz/Quiz.js
+++ models/quiz/Quiz.js
@@ -1,5 +1,19 @@
 module.exports = class Quiz {
   constructor(title) {
-    this.title = title
+    this.title = title;
+    this.questions = [];
+
+    //metadata
+    let date = new Date;
+    const dateWhenConstructorCalled = date.toUTCString();
+    this.createdDate = dateWhenConstructorCalled;
+    this.updatedDate = dateWhenConstructorCalled;
+
+    //data
+    this.quizData = {
+      favorites: 0
+    };
   }
+
+
 }
