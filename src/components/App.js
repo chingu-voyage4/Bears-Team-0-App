e094bf7d73bd6d018d00cc5521e53621780cf8c4
Louis Heimel <louisheimel1@gmail.com>
2018-02-19 19:27:35 -0600

    prettify code

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import {Route} from "react-router-dom";
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import Landing from "./Landing/Landing";
-import AllQuizzes from './AllQuizzes/AllQuizzes'
-
+import AllQuizzes from "./AllQuizzes/AllQuizzes";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/" component={Landing}/>
+        <Route exact path="/" component={Landing} />
         <Route exact path="/all" component={AllQuizzes} />
       </div>
     );
