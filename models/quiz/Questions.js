45fd66ec37b411b84fc625660026129ace057667
Kevin Galang <kgalang2@gmu.edu>
2018-02-20 05:59:27 -0800

    Add initial version of questions class

---
+++ models/quiz/Questions.js
@@ -0,0 +1,12 @@
+module.exports = class Questions {
+    constructor(question, format) {
+        this.question = question;
+        this.format = format;
+        this.options = {};
+        this.answer = "";
+        
+        this.questionData = {
+            
+        }
+    }
+}
