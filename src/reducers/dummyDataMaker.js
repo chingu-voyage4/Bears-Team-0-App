413d6b48a7074f8f20b089131152cd435605fbfb
Louis Heimel <louisheimel1@gmail.com>
2018-02-19 19:26:46 -0600

    add unique id to dummy quiz

--- src/reducers/dummyDataMaker.js
+++ src/reducers/dummyDataMaker.js
@@ -1,8 +1,10 @@
 import { range } from "lodash";
+import shortid from "shortid";
 
 export const dummyQuizzes = n => {
   return range(n).reduce((a, e, i) => {
     return a.concat({
+      id: shortid.generate(),
       title: "Title!",
       body:
         "Lorem ipsum dolor amet iceland asymmetrical DIY, hammock butcher flexitarian tbh jean shorts bicycle rights thundercats semiotics migas pour-over. Knausgaard keytar vinyl cornhole portland heirloom hexagon art party subway tile marfa dreamcatcher direct trade mlkshk. Whatever letterpress kale chips af, portland sartorial williamsburg scenester distillery celiac ennui.",
