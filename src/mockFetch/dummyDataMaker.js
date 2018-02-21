0472d5a7597d813292cb3bbff6ddaa056e135370
Louis Heimel <louisheimel1@gmail.com>
2018-02-21 15:17:27 -0600

    shorten dummy text

--- src/mockFetch/dummyDataMaker.js
+++ src/mockFetch/dummyDataMaker.js
@@ -7,7 +7,7 @@ export const dummyQuizzes = n => {
       id: shortid.generate(),
       title: "Title!",
       body:
-        "Lorem ipsum dolor amet iceland asymmetrical DIY, hammock butcher flexitarian tbh jean shorts bicycle rights thundercats semiotics migas pour-over. Knausgaard keytar vinyl cornhole portland heirloom hexagon art party subway tile marfa dreamcatcher direct trade mlkshk. Whatever letterpress kale chips af, portland sartorial williamsburg scenester distillery celiac ennui.",
+        "Lorem ipsum dolor amet iceland asymmetrical DIY, hammock butcher flexitarian",
       likes: 10
     });
   }, []);
