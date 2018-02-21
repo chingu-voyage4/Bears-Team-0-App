0a886dcdd878870fd4d74c0cca492cecc704cffe
Louis Heimel <louisheimel1@gmail.com>
2018-02-21 15:16:36 -0600

    add className of 'quiz'

--- src/components/AllQuizzes/Quiz.js
+++ src/components/AllQuizzes/Quiz.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 class Quiz extends Component {
   render() {
     return (
-      <div>
+      <div className="quiz">
         <h1>{this.props.title}</h1>
         <p>{this.props.body}</p>
         <p>
-          Likes: {this.props.likes} <a>Share</a>{" "}
+          Likes: {this.props.likes} <a href="#">Share</a>{" "}
         </p>
       </div>
     );
