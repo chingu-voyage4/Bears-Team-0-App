74237b55dbf0a62db6323c407f1fd3c254568cfc
Louis Heimel <louisheimel1@gmail.com>
2018-02-25 00:29:58 -0600

    Add Layout higher order component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import Layout from "./Layout/Layout";
 import Landing from "./Landing/Landing";
 import AllQuizzes from "./AllQuizzes/AllQuizzes";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/all" component={AllQuizzes} />
-      </div>
+      <Layout>
+        <div className="App">
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/all" component={AllQuizzes} />
+        </div>
+      </Layout>
     );
   }
 }
