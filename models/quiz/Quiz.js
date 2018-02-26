d0a208d7c79b3883575973f20872dbd20e1ce0d2
Kevin Galang <kgalang2@gmu.edu>
2018-02-26 08:19:12 -0500

    Simplify quiz model for MVP

--- models/quiz/Quiz.js
+++ models/quiz/Quiz.js
@@ -4,22 +4,13 @@ module.exports = class Quiz {
   constructor(title) {
     this.title = title;
     this.questions = [];
+    this.favorites = 0;
 
     //metadata
     if (!this.createdDate) {
       this.createdDate = generateCurrentDate();
     }
     this.updatedDate = generateCurrentDate();
-
-    //data
-    this.quizData = {
-      favorites: 0,
-      totalAttempts: 0,
-      correctAttempts: 0,
-      incorrectAttempts: 0,
-      datesUsed: [],
-      datesUpdated: [],
-    };
   }
   
   addQuestion(question) {
