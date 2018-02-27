c0610d1875881a02ffe26b313f5c225df19ff14b
Louis Heimel <louisheimel1@gmail.com>
2018-02-27 02:27:55 -0600

    import fontawesome

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
 import "./stylesheets/main.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
