d0d419f3ccd037f34e43c5fdbb0e60e0852d45d2
Vannya <van.tabbert@gmail.com>
2018-03-05 11:20:18 -0600

    /all styling refactor for mobile readiness

--- src/components/AllQuizzes/Quiz.js
+++ src/components/AllQuizzes/Quiz.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
-class Quiz extends Component {
-  render() {
-    return (
-      <div className="quiz">
-        <h1>{this.props.title}</h1>
-        <p>{this.props.body}</p>
-        <p>
-          <span>
-            <i className="fa fa-heart" /> {this.props.likes}
-          </span>{" "}
-          <a href="#">Share</a>{" "}
-        </p>
-      </div>
-    );
-  }
-}
+
+const Quiz = ({ title, mainColor, body, likes }) => {
+  return (
+    <div className="quiz">
+      <h2>{title}</h2>
+      <section className="quiz-body">
+        <p>{body}</p>
+      </section>
+      <section className={`quiz-footer ${mainColor}`}>
+        <span>
+          <i className="fa fa-heart" /> {likes}
+        </span>{" "}
+        <a href="#">Share</a>{" "}
+      </section>
+    </div>
+  );
+};
 
 export default Quiz;
