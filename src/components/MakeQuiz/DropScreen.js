69f4f5f6f12d636b3fb5719abf61e275ac0d21cd
Louis Heimel <louisheimel1@gmail.com>
2018-03-06 11:01:39 -0600

    configure drop screen

--- src/components/MakeQuiz/DropScreen.js
+++ src/components/MakeQuiz/DropScreen.js
@@ -1,18 +1,45 @@
 import React, { Component } from "react";
-
-export class DropScreen extends Component {
+import { DropTarget } from "react-dnd";
+import { connect } from "react-redux";
+import types from "./types";
+const spec = {
+    drop() {
+      return { name: "builder" };
+    }
+  },
+  collect = (connect, monitor) => {
+    return {
+      hovered: monitor.isOver(),
+      connectDropTarget: connect.dropTarget()
+    };
+  };
+class DropScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return (
+    const { questions, hovered, connectDropTarget } = this.props;
+    console.log("questions are: ", questions);
+    return connectDropTarget(
       <div
         style={{
           color: "#fff",
-          backgroundColor: "#c7c7c7",
+          backgroundColor: hovered ? "darkslategray" : "#c7c7c7",
           width: "50%",
           flexBasis: "auto"
         }}
       >
         <p>DropScreen</p>
+        <div>
+          {questions.map(question => <code>{JSON.stringify(question)}</code>)}
+        </div>
       </div>
     );
   }
 }
+export default connect(
+  state => ({
+    questions: state.makeQuizzes.questions
+  }),
+  {}
+)(DropTarget(types.BOX, spec, collect)(DropScreen));
