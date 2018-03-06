7220b97df3d48f0ada3028b89467ad99ab7ba8ce
Louis Heimel <louisheimel1@gmail.com>
2018-03-06 11:05:25 -0600

    change the way drag and drop are rendered

--- src/components/MakeQuiz/MakeQuiz.js
+++ src/components/MakeQuiz/MakeQuiz.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { DragDropContextProvider } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
 import { DragScreen } from "./DragScreen";
-import { DropScreen } from "./DropScreen";
+import DropScreen from "./DropScreen";
 import DecoratedBox from "./Box";
 
 export class MakeQuiz extends Component {
