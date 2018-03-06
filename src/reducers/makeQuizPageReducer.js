f519b7cb8cee721439fc5f839c48160382c8c489
Louis Heimel <louisheimel1@gmail.com>
2018-03-06 16:46:10 -0600

    create case to add option to multiple choice question

--- src/reducers/makeQuizPageReducer.js
+++ src/reducers/makeQuizPageReducer.js
@@ -1,5 +1,8 @@
-import { makeQuizTypes } from "../actions/types";
+import { makeQuizTypes, multipleChoiceTypes } from "../actions/types";
+import shortid from "shortid";
+
 const { ADD_MULTIPLE_CHOICE } = makeQuizTypes;
+const { ADD_OPTION } = multipleChoiceTypes;
 const initialState = {
   title: "",
   description: "",
@@ -13,13 +16,35 @@ export default (state = initialState, action) => {
         questions: [
           ...state.questions,
           {
+            id: shortid.generate(),
             question: "",
             format: "multiple choice",
-            options: {},
+            options: [],
             answer: ""
           }
         ]
       };
+    case ADD_OPTION:
+      console.log("adding option in reducer");
+      console.log("state is, ", state);
+      return {
+        ...state,
+        questions: state.questions.map(question => {
+          // find the question which matches the given id
+          return question.id === action.index
+            ? // add a new option if this is the right question
+              {
+                ...question,
+                options: [
+                  ...question.options,
+                  { val: "", id: shortid.generate() }
+                ]
+              }
+            : // else return the quiz already in place
+              { ...question };
+        })
+      };
+
     default:
       return state;
   }
