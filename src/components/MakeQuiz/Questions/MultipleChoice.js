18f992c8514bdb3413842cb877d5190fc0278494
Louis Heimel <louisheimel1@gmail.com>
2018-03-07 22:03:58 -0600

    add more functionality to multiple choice question

--- src/components/MakeQuiz/Questions/MultipleChoice.js
+++ src/components/MakeQuiz/Questions/MultipleChoice.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
-import { addOption } from "../../../actions/multipleChoice";
+import Option from "./Option";
+import {
+  addOption,
+  changeOption,
+  toggleCorrectOption
+} from "../../../actions/multipleChoice";
 import { connect } from "react-redux";
 
 class MultipleChoice extends Component {
   render() {
-    const { questionData, addOption } = this.props;
+    const { questionData, addOption, changeOption } = this.props;
     const { options, id } = questionData;
     return (
       <div>
-        <code style={{ display: "block" }}>{JSON.stringify(questionData)}</code>
+        {/*<code style={{ display: "block" }}>{JSON.stringify(questionData)}</code>*/}
         <input placeholder="Question" />
         <button onClick={() => addOption(id)}>Add Option</button>
-        {options.map(option => <p key={option.id}>{option.val}</p>)}
+        {options.map(option => (
+          <Option
+            key={option.id}
+            change={e => changeOption(id, option.id, e)}
+            toggleCorrectOption={() => toggleCorrectOption(id, option.id)}
+            id={option.id}
+            val={option.val}
+          />
+        ))}
       </div>
     );
   }
 }
 export default connect(state => ({}), {
-  addOption
+  addOption,
+  changeOption,
+  toggleCorrectOption
 })(MultipleChoice);
