2433d0e1cd5b19c37d7c4e9d034cd0bac1d39f86
Kevin Galang <kgalang2@gmu.edu>
2018-03-13 12:18:01 -0400

    Start using BDD syntax

--- routes/__tests__/quiz_routes.test.js
+++ routes/__tests__/quiz_routes.test.js
@@ -2,9 +2,17 @@ const request = require("supertest");
 const api = require("../../server");
 
 describe("Quiz Routes", () => {
-    test("GET all quizzes", async() => {
-        const response = await request(api).get('/api/quizzes/all');
-        expect(response.statusCode).toBe(200);
-        expect(response.body.quizzes).toBeInstanceOf(Array);
-    });
+
+        it("gets all quizzes", async () => {
+            const response = await request(api).get('/api/quizzes/all');
+            expect(response.statusCode).toBe(200);
+            expect(response.body.quizzes).toBeInstanceOf(Array);
+        });
+
+        it("gets count of quizzes", async () => {
+            const response = await request(api).get("/api/quizzes/count");
+            expect(response.statusCode).toBe(200);
+            expect(response.body.count).toBeGreaterThanOrEqual(0);
+        });
+
 });
