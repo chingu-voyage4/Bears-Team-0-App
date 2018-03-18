f7eab22d5e98c1e4ac296f9a39e8c3d36ce1af74
Louis Heimel <louisheimel1@gmail.com>
2018-03-18 15:58:07 -0500

    get rid of unneccesary imports

--- src/components/MakeQuiz/Questions/Dropdown.js
+++ src/components/MakeQuiz/Questions/Dropdown.js
@@ -1,15 +1,5 @@
 import React, { Component } from "react";
-import Delete from "./Delete";
-import Option from "./Option";
 import OptionQuestion from "./OptionQuestion";
-import {
-  addOption,
-  changeOption,
-  toggleCorrectOption,
-  changeQuestion,
-  deleteOption
-} from "../../../actions/multipleChoice";
-import { connect } from "react-redux";
 
 class Dropdown extends Component {
   render() {
--- src/components/MakeQuiz/Questions/MultipleChoice.js
+++ src/components/MakeQuiz/Questions/MultipleChoice.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-import Delete from "./Delete";
-import Option from "./Option";
 import OptionQuestion from "./OptionQuestion";
-import { connect } from "react-redux";
 
 class MultipleChoice extends Component {
   render() {
