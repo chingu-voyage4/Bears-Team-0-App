a2fc6837c2c928a0011ecbc22ab489580949634a
Louis Heimel <louisheimel1@gmail.com>
2018-03-20 13:41:03 -0500

    add quizzes action tests

--- src/actions/test/quizzes.test.js
+++ src/actions/test/quizzes.test.js
@@ -1,3 +1,31 @@
-describe("", () => {
-  it("", () => {});
+import {
+  requestAllQuizzes,
+  receiveAllQuizzes,
+  requestYourQuizzes
+} from "../quizzes";
+
+describe("quiz actions", () => {
+  it("should request all quizzes", () => {
+    expect(requestAllQuizzes()).toEqual({ type: "REQUEST_ALL_QUIZZES" });
+  });
+
+  it("should receive all quizzes", () => {
+    const quizzes = {
+      foo: "foo",
+      bar: "bar"
+    };
+    expect(receiveAllQuizzes(quizzes)).toEqual({
+      type: "RECEIVE_ALL_QUIZZES",
+      payload: { foo: "foo", bar: "bar" }
+    });
+  });
+
+  it("should request your quizzes", () => {
+    const userId = 1;
+
+    expect(requestYourQuizzes(userId)).toEqual({
+      type: "REQUEST_YOUR_QUIZZES",
+      userId: 1
+    });
+  });
 });
