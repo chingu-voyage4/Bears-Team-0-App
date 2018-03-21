5af0fd70bdf1528e75e21f137616eae00befc8bd
Kevin Galang <kgalang2@gmu.edu>
2018-03-21 11:49:37 -0400

    Add test to get count of users.

--- routes/__tests__/user_routes.test.js
+++ routes/__tests__/user_routes.test.js
@@ -5,9 +5,15 @@ describe("User Routes", () => {
 
   it("gets all users through route", async () => {
     const response = await request(api).get('/api/users/');
-    console.warn(response.body);
     expect(response.statusCode).toBe(200);
     expect(response.body.users).toBeInstanceOf(Array);
+  });
+
+  it("gets count of users through route", async () => {
+    const response = await request(api).get("/api/users/count");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.count).toBeGreaterThanOrEqual(0);
+    console.log(response.body.count);
   });
 
 });
