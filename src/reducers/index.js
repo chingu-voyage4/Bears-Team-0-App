df4ba8e98b6e20d7548b2bdecaaac25b4eb2703f
Louis Heimel <louisheimel1@gmail.com>
2018-03-28 17:46:42 -0500

    add title and description reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,10 @@ import allQuizzesPageReducer from "./allQuizzesPageReducer";
 import login from "./login";
 import makeQuizPageReducer from "./makeQuizPageReducer";
 import takeQuizPageReducer from "./takeQuizPageReducer";
+import titleAndDescription from "./titleAndDescription";
 
 export default combineReducers({
+  titleAndDescription: titleAndDescription,
   allQuizzes: allQuizzesPageReducer,
   makeQuizzes: makeQuizPageReducer,
   takeQuizzes: takeQuizPageReducer,
