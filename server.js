8db6866ec119916e59b5ed12ef9ba82feac37892
Louis Heimel <louisheimel1@gmail.com>
2018-03-28 04:00:06 -0500

    enable cors

--- server.js
+++ server.js
@@ -1,39 +1,49 @@
-const express       = require("express");
-const compression   = require("compression");
-const bodyParser    = require("body-parser");
-const morgan        = require("morgan");
-const config        = require("./config");
+const express = require("express");
+const compression = require("compression");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const config = require("./config");
 
-const log           = require('debug')('api:server');
-const error         = require('debug')('api:error');
+const log = require("debug")("api:server");
+const error = require("debug")("api:error");
 
-const router        = require('./routes');
-const auth          = require("./auth");
+const router = require("./routes");
+const auth = require("./auth");
 
 const apiServer = express();
 /*
 Global Middleware
 */
 apiServer.use(compression());
-apiServer.use(morgan('dev'));
-apiServer.use(bodyParser.urlencoded({extended : true}));
+apiServer.use(morgan("dev"));
+apiServer.use(bodyParser.urlencoded({ extended: true }));
 apiServer.use(bodyParser.json());
+
+apiServer.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 /*
 Passport initialization
 */
-auth.initPassport(apiServer)
+auth.initPassport(apiServer);
 /*
 API router
 */
-apiServer.use('/auth', auth.router);
-apiServer.use('/api', router);
+apiServer.use("/auth", auth.router);
+apiServer.use("/api", router);
 /*
 Global error handling
 */
 apiServer.use((err, req, res, next) => {
-    error((err.status || 500) + " " + error.message)
-    // Handle error?
-    res.status(err.status || 500).json({ error: err.message, trace: err.stack })
-})
+  error((err.status || 500) + " " + error.message);
+  // Handle error?
+  res.status(err.status || 500).json({ error: err.message, trace: err.stack });
+});
 
-module.exports = apiServer
+module.exports = apiServer;
