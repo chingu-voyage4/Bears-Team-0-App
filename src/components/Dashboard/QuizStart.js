dd8f5885dc3b07fc92e8027efd7b98badf63199d
Louis Heimel <louisheimel1@gmail.com>
2018-03-28 17:38:38 -0500

    wrap connected QuizStart component inside HOC withRouter

--- src/components/Dashboard/QuizStart.js
+++ src/components/Dashboard/QuizStart.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import {
   changeTitle,
   changeDescription,
@@ -9,6 +10,7 @@ import { connect } from "react-redux";
 // this component collects the title and description for a started quiz
 class QuizStart extends Component {
   render() {
+    const { submit, title, description, history } = this.props;
     return (
       <form className="quiz-start">
         <h1>Quiz Maker</h1>
@@ -16,16 +18,16 @@ class QuizStart extends Component {
           <label htmlFor="quiz-title">Quiz Title</label>
           <input
             id="quiz-title"
-            value={this.props.title}
+            value={title}
             onChange={this.props.changeTitle}
           />
           <label htmlFor="quiz-description">Description (Optional)</label>
           <textarea
             id="quiz-description"
-            value={this.props.description}
+            value={description}
             onChange={this.props.changeDescription}
           />
-          <button type="button" onClick={this.props.submit}>
+          <button type="button" onClick={() => submit(history)}>
             Make A Quiz
           </button>
         </div>
@@ -34,14 +36,19 @@ class QuizStart extends Component {
   }
 }
 
-export default connect(
-  state => ({
-    title: state.allQuizzes.newQuiz.title,
-    description: state.allQuizzes.newQuiz.description
-  }),
-  dispatch => ({
-    changeTitle: e => dispatch(changeTitle(e)),
-    changeDescription: e => dispatch(changeDescription(e)),
-    submit: () => dispatch(submitQuizStart())
-  })
-)(QuizStart);
+export default withRouter(
+  connect(
+    state => ({
+      title: state.titleAndDescription.title,
+      description: state.titleAndDescription.description
+    }),
+    dispatch => ({
+      changeTitle: e => dispatch(changeTitle(e)),
+      changeDescription: e => dispatch(changeDescription(e)),
+      submit: history => {
+        history.push("/makeQuiz");
+        dispatch(submitQuizStart());
+      }
+    })
+  )(QuizStart)
+);
